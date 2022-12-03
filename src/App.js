c963302e364e5f93a876cf79316037541b96628e
Dwi Nurhadi A <dwi.nurhadi17@student.uns.ac.id>
2022-12-03 15:02:13 +0700

    add new home dir

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <p>Hellow github action</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
